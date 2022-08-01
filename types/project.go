0a4df9fec6c2fb707b6af44095b360df137672f4
Laura Brehm <laurabrehm@hey.com>
2022-08-02 00:51:54 +0200

    WIP

--- types/project.go
+++ types/project.go
@@ -36,7 +36,7 @@ type Project struct {
 	Volumes      Volumes           `yaml:",omitempty" json:"volumes,omitempty"`
 	Secrets      Secrets           `yaml:",omitempty" json:"secrets,omitempty"`
 	Configs      Configs           `yaml:",omitempty" json:"configs,omitempty"`
-	Extensions   Extensions        `yaml:",inline" json:"-"` // https://github.com/golang/go/issues/6213
+	Extensions   Extensions        `yaml:",inline" json:",inline"` // https://github.com/golang/go/issues/6213
 	ComposeFiles []string          `yaml:"-" json:"-"`
 	Environment  map[string]string `yaml:"-" json:"-"`
 
